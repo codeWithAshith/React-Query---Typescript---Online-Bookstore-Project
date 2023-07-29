4339cd8547d22cef4844819b790f4961aef30082
codewithashith <codewithashith@gmail.com>
2023-07-29 18:07:14 +0530

    04. Offer section

--- src/components/Button.component.tsx
+++ src/components/Button.component.tsx
@@ -18,8 +18,8 @@ const ButtonComponent: React.FC<Props> = ({
       {...rest}
       className={`border rounded-3xl p-2 px-6 ${
         variant === "default"
-          ? "border-slate-50 bg-slate-900 text-white "
-          : "border-slate-900 bg-slate-100 text-slate-950"
+          ? "border-slate-50 bg-slate-900 text-white hover:bg-slate-100 hover:text-slate-900 hover:border-slate-900"
+          : "border-slate-900 bg-slate-100 text-slate-950 hover:bg-slate-900 hover:text-slate-50 hover:border-slate-50"
       } ${className}`}
       onClick={onClick}
     >
--- src/components/SpecialOffer.component.tsx
+++ src/components/SpecialOffer.component.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import Lottie from "lottie-react";
+import offer from "../images/offer.json";
+import ButtonComponent from "./Button.component";
+import { useNavigate } from "react-router";
 
 const SpecialOfferComponent = () => {
-  return <div>SpecialOfferComponent</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex min-h-[100vh] bg-slate-900">
+      <Lottie className="flex-1" animationData={offer} loop={true} />
+      <div className="flex-1 flex flex-col justify-center px-8">
+        <p className="text-slate-50 text-3xl mb-6">Special Offers </p>
+        <p className="text-red-500 text-6xl mb-4">Books 50% off now! </p>
+        <p className="text-red-500 text-6xl mb-4">Don't miss such a deal! </p>
+        <p className="text-slate-50 text-sm mb-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+          voluptatum quas ipsam totam laudantium animi quaerat voluptatibus
+          suscipit officiis nemo.
+        </p>
+        <ButtonComponent
+          label="Explore"
+          onClick={() => {
+            navigate("/shop");
+          }}
+          className="px-16 text-2xl self-start"
+          variant="outlined"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SpecialOfferComponent;
